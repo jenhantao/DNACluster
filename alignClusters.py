731f2af1e207fb7c315ced0c9c9d96a0f3c3aa6e
Jenhan Tao <jenhan@jenhan-1005PE.(none)>
2012-09-18 17:46:27 -0700

    script can extract certain clusters

--- alignClusters.py
+++ alignClusters.py
@@ -19,9 +19,48 @@ if len(sys.argv) < 3:
 
 filePath = str(sys.argv[1])
 tokens = sys.argv[2][1:len(sys.argv[2])-1].split(",")
-lowerBound = float(tokens[0])
-upperBound = float(tokens[1])
-if sys.argv[3] != "f" 
+if len(tokens[0]) > 0:
+   lowerBound = float(tokens[0]) # lower bound of the interval for cluster size
+else:
+   lowerBound = 0;
+if len(tokens[1]) > 0:
+   upperBound = float(tokens[1]) # upper bound of the interval for cluster size
+else:
+   upperBound = 999999 # arbitrarily large value
+outputFile = False # whether or not output files should be created
+outputFileDirectory = "./"# directory where files will be created
+if sys.argv[3] != "f" and sys.argv[3] != "p":
+   outputFile = False
+elif sys.argv[3] == "f":
+   outputFile = True
 
+if len(sys.argv) > 4:
+   outputFileDirectory = str(sys.argv[4]);
 
+# Create output directory if it doesn't exist
+if not os.path.exists(outputFileDirectory) and outputFile:
+    os.makedirs(outputFileDirectory)
 
+print "input file: " + filePath
+print "interval: " + str(lowerBound) + "<= n <= " + str(upperBound)
+print "output file?: " + str(outputFile)+ " to: " + outputFileDirectory
+
+with open(filePath) as f:
+     inputFileLines = f.readlines()
+
+clusterCount = 0
+numFiles = 0;
+for line in inputFileLines:
+   line = line.rstrip("\n")
+   if line.startswith("#"):
+      numFiles = numFiles +1
+      numberInCluster = float(line.rstrip("\n")[1:])
+      if (numberInCluster >= lowerBound and numberInCluster <= upperBound) and outputFile:
+         file = open(outputFileDirectory+"/cluster_"+str(clusterCount)+".fasta", 'w+')
+         clusterCount = clusterCount +1
+   elif numberInCluster >= lowerBound and numberInCluster <= upperBound:
+      if len(line)>0:
+          if outputFile:
+             file.write(line+"\n")
+          else:
+             print(line)
